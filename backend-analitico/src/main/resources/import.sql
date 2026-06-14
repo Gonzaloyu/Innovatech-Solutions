af6ffef572e74153579125472065372562095d64
Shishu <lu.garridou@duocuc.cl>
2026-06-14 19:42:59 -0400

    Se agrego los 15 empleados y se arreglo el backend notificacion

---
+++ backend-analitico/src/main/resources/import.sql
@@ -0,0 +1,23 @@
+INSERT INTO departamentos (nombre) VALUES ('TI');
+INSERT INTO departamentos (nombre) VALUES ('Ventas');
+INSERT INTO departamentos (nombre) VALUES ('Consultoría');
+
+INSERT INTO cargos (nombre, nivel) VALUES ('Desarrollador', 'Junior');
+INSERT INTO cargos (nombre, nivel) VALUES ('Desarrollador', 'Senior');
+INSERT INTO cargos (nombre, nivel) VALUES ('Arquitecto', 'Senior');
+
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Benjamin Ojeda', 'benjamin.ojeda@innovatech.cl', 1, 1);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Gino Trujillo', 'gino.trujillo@innovatech.cl', 1, 2);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Samuel Deluque', 'samuel.deluque@innovatech.cl', 3, 3);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Guillermo Díaz', 'guillermo.diaz@innovatech.cl', 1, 2);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Renato Astete', 'renato.astete@innovatech.cl', 2, 1);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Franko Sanchez', 'franko.sanchez1@innovatech.cl', 1, 3);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Tiziano Piana', 'tiziano.piana@innovatech.cl', 3, 1);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Nicky Jam', 'nicky.jam@innovatech.cl', 2, 1);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Elvis Crespo', 'elvis.crespo@innovatech.cl', 2, 2);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Carlos Contreras', 'carlos.contreras@innovatech.cl', 3, 1);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Daddy Yankee', 'daddy.yankee@innovatech.cl', 1, 3);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Rene Puente', 'rene.puente@innovatech.cl', 2, 1);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Miguel Cornejo', 'miguel.cornejo@innovatech.cl', 3, 2);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Alexis Sanchez', 'alexis.sanchez@innovatech.cl', 2, 1);
+INSERT INTO empleados (nombre, email, departamento_id, cargo_id) VALUES ('Fabian Ortega', 'fabian.ortega@innovatech.cl', 1, 2);
